dde4f5713ac1d251525ab4a657b9002553946fca
James <N3rdP1um23@users.noreply.github.com>
2019-03-26 15:43:26 -0400

    Added PowerShell Script for Windows

---
+++ LazyMan_IP_Auto_Updater.ps1
@@ -0,0 +1,31 @@
+#################################################
+#                                               #
+#               N3rdP1um23                      #
+#             March 26, 2019                    #
+#            Lazyman IP Update                  #
+#                                               #
+#       This script is used to update           #
+#      the Lazyman IPs in the hosts file        #
+#                                               #
+#################################################
+
+
+## Variables
+$HostFile = "$($env:windir)\system32\Drivers\etc\hosts"
+$LazymanURL = "powersports.ml"
+$LazymanHostsIP = ""
+$LazymanCurrentIP = ""
+
+## Grab the current stored IP address and ping for the current one
+$LazymanHostsIP = Get-Content $HostFile | ForEach-Object { if($_ -match $LazymanURL) { $_.replace("#$($LazymanURL)-", "") }}
+$LazymanCurrentIP = (Test-Connection $LazymanURL -count 1).IPV4ADDRESS.IPAddressToString
+
+## Check if the Current IP is different than the stored one
+if ($LazymanCurrentIP -ne $LazymanHostsIP) {
+    ## Update the Hosts File with the new IP
+    Write-Host "Updates Are Being Applied"
+    (Get-Content $HostFile).replace($LazymanHostsIP, $LazymanCurrentIP) | Set-Content .\test.txt
+}else{
+    ## Display That the script is not updating
+    Write-Host "No Updates To Be Performed"
+}
